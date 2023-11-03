b0b9a4c2560a3c3794a096f2bdd447ac7182613d
AlejandroAlberoni <atralberoni@inf.ufpel.edu.br>
2023-11-03 03:10:31 -0300

    fixing layout -> new brainstorming

--- app/components/hellopresentation.tsx
+++ app/components/hellopresentation.tsx
@@ -7,8 +7,8 @@ const HelloPresentation = () => {
   },
     [])
   return (
-    <div className='flex border-2 border-blue-500'>
-        <div className='p-6 w-min text-center float-left align-middle border-2 border-emerald-100'>
+    <div className='flex'>
+        <div className='p-6 w-min text-center float-left align-middle '>
           <div className='p-10 text-justify'>
             <h2 className='font-lexendexa font-bold text-4xl tracking-widest antialiased'>Hello There.</h2>
           </div>
--- app/components/midpage.tsx
+++ app/components/midpage.tsx
@@ -1,9 +1,13 @@
+'use client';
 import React from 'react'
 
 const MidPage = () => {
   return (
-    <div className='border-2 border-black '>
-      <div className='flex mr-20 float-right text-right w-[400px] min-h-[200px] font-Inter font-semibold text-[35px] leading-relaxed'>
+    <div className='flex justify-between'>
+      <div className='float-left'>
+          OI
+      </div>
+      <div className='p-4 border-8 border-black inline-block mr-20 float-right text-right w-[400px] min-h-[200px] font-Inter font-semibold text-[35px] leading-relaxed'>
           This project is intended to be a front-end portfolio. Brainstormed the design using Figma. It is supposed to have some various stuff.
       </div>
     </div>
