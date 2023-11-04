2481e71986ea203882d81f3ed9ee7ad6ec779fce
AlejandroAlberoni <atralberoni@inf.ufpel.edu.br>
2023-11-04 00:45:14 -0300

    fix: just a quick layout fix

--- app/components/midpage.tsx
+++ app/components/midpage.tsx
@@ -6,7 +6,7 @@ const MidPage = () => {
 
   return (
     <div className='flex inset-x-0 place-content-end'>
-      <div className='p-4 mr-36 text-right w-2/4 min-h-[200px] font-Inter font-semibold text-[35px] leading-relaxed'>
+      <div className='p-4 mr-36 text-right w-1/2 min-h-[300px] font-Inter font-semibold text-[35px] leading-relaxed'>
           This project is intended to be a front-end portfolio. Brainstormed the design using Figma. It is supposed to have some various stuff.
       </div>
 
--- app/components/top_page.tsx
+++ app/components/top_page.tsx
@@ -11,8 +11,8 @@ const TopWrapper = () => {
       <div className='w-min p-24 text-justify'>
         <h2 className='font-lexendexa font-bold text-4xl tracking-widest antialiased'>Hello There.</h2>
       </div>
-      <div className='absolute mt-20 ml-48 h-36 w-36 z-[-2] rounded-full bg-[#8A7A7A]' />
-      <div className='opacity-75 absolute mt-28 ml-40 h-36 w-36 z-[-3] rounded-full bg-[#8A7A7A]' />
+      <div className='absolute mt-20 ml-48 h-40 w-40 z-[-2] rounded-full bg-[#8A7A7A]' />
+      <div className='absolute opacity-60 mt-28 ml-40 h-48 w-48 z-[-3] rounded-full bg-[#8A7A7A]' />
     </div>
   )
 }
