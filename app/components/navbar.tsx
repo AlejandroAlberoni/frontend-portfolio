12d4af97d5b438394a6ec6b862cfee54b597f742
AlejandroAlberoni <atralberoni@inf.ufpel.edu.br>
2023-11-08 18:30:33 -0300

    feat: navigation bar button styles

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 const NavigationContext = () => {
   return (
-    <div className='sm:flex w-full h-auto justify-between p-4 sm:p-12 text-center border-b-[2px] border-black'>
+    <div className='sm:flex w-full h-auto justify-between p-4 sm:p-12 text-center items-center'>
       <div className='m-4 sm:m-0'>
         <h2 className='lg:animate-blurred-fade-in font-lexendexa font-bold text-lg tracking-widest antialiased'>A.T.R.A</h2>
       </div>
-      <div className='animate-expand-vertically sm:animate-none grid sm:flex space-x-3 sm:space-x-6 font-Staatliches text-xl sm:tracking-[0.2em] tracking-normal'>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/"}>Projects</Link>
-        <Link href={"/"}>Academic</Link>
-        <Link href={"/"}>Author</Link>
-        <Link href={"/"}>Contact</Link>
+      <div className='grid sm:flex sm:place-items-baseline mt:animate-expand-vertically sm:animate-none space-x-3 sm:space-x-8 font-Staatliches text-xl sm:tracking-[0.2em] tracking-normal'>
+        <Link className='p-3 hover:border-slate-200 hover:rounded-[6px] duration-500 hover:bg-black hover:text-white' href={"/"}>Home</Link>
+        <Link className='p-3 hover:border-slate-200 hover:rounded-[6px] duration-500 hover:bg-black hover:text-white' href={"/"}>Projects</Link>
+        <Link className='p-3 hover:border-slate-200 hover:rounded-[6px] duration-500 hover:bg-black hover:text-white' href={"/"}>Academic</Link>
+        <Link className='p-3 hover:border-slate-200 hover:rounded-[6px] duration-500 hover:bg-black hover:text-white' href={"/"}>Author</Link>
+        <Link className='p-3 hover:border-slate-200 hover:rounded-[6px] duration-500 hover:bg-black hover:text-white' href={"/"}>Contact</Link>
       </div>
     </div>
   )
