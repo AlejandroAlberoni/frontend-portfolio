3bcd8ecba3e8f68bc2c1b7aa80c6d6f815b42c7e
AlejandroAlberoni <atralberoni@inf.ufpel.edu.br>
2023-11-11 23:57:51 -0300

    fix: resp. design little fixes

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 const NavigationContext = () => {
   return (
-    <div className='sm:flex w-full h-auto justify-between p-4 sm:p-12 text-center items-center'>
+    <div className='sm:flex w-full h-auto sm:justify-between p-4 sm:p-12 text-center items-center'>
       <div className='m-4 sm:m-0'>
-        <h2 className='lg:animate-blurred-fade-in font-lexendexa font-bold text-lg tracking-widest antialiased'>A.T.R.A</h2>
+        <h2 className='animate-blurred-fade-in font-lexendexa font-bold text-lg tracking-widest antialiased'>A.T.R.A</h2>
       </div>
-      <div className='grid sm:flex sm:place-items-baseline mt:animate-expand-vertically sm:animate-none space-x-3 sm:space-x-8 font-Staatliches text-xl sm:tracking-[0.2em] tracking-normal'>
+      <div className='grid sm:flex sm:place-items-baseline animate-expand-vertically space-x-3 sm:space-x-8 font-Staatliches text-xl sm:tracking-[0.2em] tracking-normal'>
         <Link className='sm:p-3 hover:rounded-[6px] duration-500 hover:bg-black hover:text-white hover:shadow-2xl' href={"/"}>Home</Link>
         <Link className='sm:p-3 hover:rounded-[6px] duration-500 hover:bg-black hover:text-white hover:shadow-2xl' href={"/"}>Projects</Link>
         <Link className='sm:p-3 hover:rounded-[6px] duration-500 hover:bg-black hover:text-white hover:shadow-2xl' href={"/"}>Academic</Link>
