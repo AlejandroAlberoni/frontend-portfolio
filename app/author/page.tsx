155e185f9e9e5fdd202bc3db3ba4c4d35a9d051c
AlejandroAlberoni <atralberoni@inf.ufpel.edu.br>
2023-11-23 00:27:14 -0300

    a

--- app/author/page.tsx
+++ app/author/page.tsx
@@ -9,23 +9,21 @@ const show_attributtes = [
 ]
 const AuthorPage = () => {
   return (
-    <div className='flex flex-col my-[10vh]'>
+    <div className='flex flex-col my-8 md:my-[10vh]'>
       <div className='grid place-items-center gap-y-2'>
-        <Image className='' src={`/profile.png`} width={250} height={250} alt='profile_art'></Image>
+        <Image className='w-[150px] h-[150px] md:w-[250px] md:h-[250px]' src={`/profile.png`} width={250} height={250} alt='profile_art'></Image>
         <p className='font-Kalam font-bold text-[20px] text-center'>A.T.R.A.</p>
-        <div className='grid'>
-          <h2 className='block font-lexendexa text-[25px] font-semibold'>Social media</h2>
-          <div className='flex justify-center space-x-3 p-3'>
-            <a href='https://www.linkedin.com/in/alejandro-alberoni-8b95961b4' target='_blank'><Image className='transition ease-in-out duration-300 hover:bg-blue-400' src={'/logotipo-do-linkedin.png'} alt={'LinkedIN social media'} width={20} height={20}></Image></a>
-            <a href='https://www.instagram.com/atralberoni/' target='_blank' ><Image className='transition ease-in-out duration-300 hover:bg-blue-500 rounded-[5px]' src={'/logotipo-do-instagram.png'} alt={'Instagram social media'} width={20} height={20}></Image></a>
-          </div>
+        <h2 className='font-lexendexa text-[15px] md:text-[25px] font-semibold'>Social media</h2>
+        <div className='flex justify-center space-x-4'>
+          <a href='https://www.linkedin.com/in/alejandro-alberoni-8b95961b4' target='_blank'><Image className='transition ease-in-out duration-300 hover:bg-blue-400' src={'/logotipo-do-linkedin.png'} alt={'LinkedIN social media'} width={20} height={20}></Image></a>
+          <a href='https://www.instagram.com/atralberoni/' target='_blank' ><Image className='transition ease-in-out duration-300 hover:bg-blue-500 rounded-[5px]' src={'/logotipo-do-instagram.png'} alt={'Instagram social media'} width={20} height={20}></Image></a>
         </div>
       </div>
       <div className='grid flex-grow place-items-center gap-y-10'>
         {
           show_attributtes.map((item, index) => {
             return (
-              <div key='wrapper' className='relative h-[180px] w-[40vw]' >
+              <div key='wrapper' className='relative flex h-[180px] w-[40vw]' >
                 <div key={'rectangle1'} className='absolute flex place-items-center h-[80%] w-[70%] bg-[#a7a7a7] font-Mitr rounded-md bottom-0 left-0'>
                   <div className='left-0 -rotate-90'>
                     {item.label}
