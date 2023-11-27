880c54e8c430b0a019c750139196b50d1158eae6
AlejandroAlberoni <atralberoni@inf.ufpel.edu.br>
2023-11-26 21:43:35 -0300

    feat:  preview contact page

--- app/author/page.tsx
+++ app/author/page.tsx
@@ -13,14 +13,14 @@ const AuthorPage = () => {
       <div className='grid place-items-center gap-y-2'>
         <Image className='w-[150px] h-[150px] md:w-[250px] md:h-[250px]' src={`/profile.png`} width={250} height={250} alt='profile_art'></Image>
         <p className='font-Kalam font-bold text-[20px] text-center'>A.T.R.A.</p>
-        <h2 className='font-lexendexa text-[15px] md:text-[25px] font-semibold'>Social media</h2>
+        <h2 className='font-lexendexa text-[15px] md:text-[25px] font-semibold m-4'>Social media</h2>
         <div className='flex justify-center space-x-4'>
           <a href='https://www.linkedin.com/in/alejandro-alberoni-8b95961b4' target='_blank'><Image className='transition ease-in-out duration-300 hover:bg-blue-400' src={'/logotipo-do-linkedin.png'} alt={'LinkedIN social media'} width={20} height={20}></Image></a>
-          <a href='https://www.instagram.com/atralberoni/' target='_blank' ><Image className='transition ease-in-out duration-300 hover:bg-blue-500 rounded-[5px]' src={'/logotipo-do-instagram.png'} alt={'Instagram social media'} width={20} height={20}></Image></a>
+          <a href='https://www.instagram.com/atralberoni/' target='_blank' ><Image className='transition ease-in-out duration-300 hover:bg-blue-500 rounded-[6px]' src={'/logotipo-do-instagram.png'} alt={'Instagram social media'} width={20} height={20}></Image></a>
         </div>
       </div>
       <div className='grid flex-grow place-items-center gap-y-10'>
-        {
+        { /*
           show_attributtes.map((item, index) => {
             return (
               <div key='wrapper' className='relative flex h-[180px] w-[40vw]' >
@@ -35,7 +35,7 @@ const AuthorPage = () => {
               </div>
             )
           })
-        }
+        */ }
       </div>
     </div >
   )
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 const ContactPage = () => {
   return (
-    <div>
-        ContactPage
+    <div className=''>
+      <div className='flex flex-col justify-center items-center my-20 gap-10'>
+        <div className='font-Kalam font-bold text-[40px] tracking-widest'> Send me a <span className='text-[#4986BD] underline'>message</span>.</div>
+        <form action="" className='grid space-y-6'>
+          <input className='rounded-[12px] p-4 focus:border-[1px] focus:border-[#4986BD]' placeholder='Name'></input>
+          <input className='rounded-[12px] p-4 ' placeholder='Email'></input>
+          <input className='rounded-[12px] p-4 ' placeholder='Message'></input>
+          <div className='space-x-2 bg-[#4986BD] rounded-[23px] text-center'>
+            <button className='font-lexendexa text-white'>Submit</button>
+            <Image className='inline-block' src={'/seta_direita.png'} width={10} height={10} alt="Send info arrow"></Image>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
