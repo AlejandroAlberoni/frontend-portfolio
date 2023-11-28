50e0397476c2073c1c910c39c357d951770fc962
AlejandroAlberoni <atralberoni@inf.ufpel.edu.br>
2023-11-28 02:56:14 -0300

    a

--- app/academic/page.tsx
+++ app/academic/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 
 const Academic = () => {
+  const[openModal, setOpenModal] = useState(null)
+  const loginmodalopen = function () {
+
+  }
   return (
-    <div>Academic</div>
+    <div className='flex h-[100vh] justify-center items-center'>
+      <button className='hover:underline animate-pulse'>Something is weird here... What is this?</button>
+    </div>
   )
 }
 
